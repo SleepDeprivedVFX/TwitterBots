2e747e1e698918646f1c9aa7530d728e82c8df7c
Adam Benson <SleepDeprivedVFX@users.noreply.github.com>
2022-03-16 10:20:44 -0700

    Found out that all the tweets were marked inactive.  Need to find out why.  Monitoring it for now.

--- LittleBird.py
+++ LittleBird.py
@@ -79,6 +79,7 @@ def bird_nest():
                     # sorted_tweets = sorted(collect_tweets, key=lambda i: (i['last_posted'], i['post_count']),
                     #                        reverse=True)[1:]
                     sorted_tweets = sorted(collect_tweets, key=lambda i: (i['post_count']), reverse=True)[:15]
+                    logger.debug('SORTED TWEETS: %s' % sorted_tweets)
                     logger.info('Grabbing a pre-built tweet...')
                     get_tweet_id = brains.find_random_tweet(tweet_list=sorted_tweets)
                     logger.debug('Tweet ID: %s' % get_tweet_id)
